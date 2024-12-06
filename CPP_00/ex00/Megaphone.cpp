9b00c15fac7595bfd854c4d50512686af45b57ed
Brobit <alannmaricourt15@gmail.com>
2024-12-06 16:42:48 +0100

    megaphone done

--- CPP_00/ex00/Megaphone.cpp
+++ CPP_00/ex00/Megaphone.cpp
@@ -1,8 +1,19 @@
+#include <cctype>
 #include <iostream>
+#include <string>
 
 int	main(int argc, char **argv)
 {
 	if (argc == 1)
-		return std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
+	argv++;
+	std::locale loc;
+	for (int i = 0; argv[i]; i++) {
+		std::string string = argv[i];
+		for(std::string::size_type j = 0; j < string.length(); j++) {
+			std::cout << std::toupper(string[j], loc);
+		}
+	}
+	std::cout << std::endl;
 	return (0);
 }
