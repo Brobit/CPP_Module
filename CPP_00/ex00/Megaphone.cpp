83ed7b49115675b62e089ef7d864e52ad0802670
Brobit <alannmaricourt15@gmail.com>
2024-12-08 11:51:32 +0100

    last modif on ex00

--- CPP_00/ex00/Megaphone.cpp
+++ CPP_00/ex00/Megaphone.cpp
@@ -8,7 +8,7 @@ int	main(int argc, char **argv)
 		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
 	argv++;
 	std::locale loc;
-	for (int i = 0; argv[i]; i++) {
+	for (std::string::size_type i = 0; argv[i]; i++) {
 		std::string string = argv[i];
 		for(std::string::size_type j = 0; j < string.length(); j++) {
 			std::cout << std::toupper(string[j], loc);
