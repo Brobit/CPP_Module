ec252f23f4ca1bf239b8a55de116a802c38c58fe
Brobit <alannmaricourt15@gmail.com>
2024-12-08 21:26:41 +0100

    cpp01/ex04 done

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -16,29 +16,28 @@ bool	searchAndReplace(const std::string &filename, const std::string &s1, const 
 	if (!checkString(s1, s2))
 		return (false);
 
-	std::ifstream inputFile(filename);
+	std::ifstream inputFile(filename.c_str());
 	if (!inputFile)
 		return (std::cout << "Error occur while opening the file" << std::endl, false);
 
-	std::ofstream outputFile(filename + ".replace");
+	std::ofstream outputFile((filename + ".replace").c_str());
 	if (!outputFile)
 		return (std::cout << "Error occur while creating the output file" << std::endl, inputFile.close(), false);
 
 	std::string line;
-	std::string lineToPut;
 	while (std::getline(inputFile, line))
 	{
+		std::string lineToPut;
 		for (std::string::size_type i = 0; i < line.length(); i++) {
 			if (line.substr(i, s1.length()) == s1)
 			{
 				lineToPut += s2;
-				i += s1.length();
+				i += s1.length() - 1;
 			}
 			else
-				lineToPut += line[i++];
+				lineToPut += line[i];
 		}
 		outputFile << lineToPut << std::endl;
-		lineToPut.clear();
 	}
 	return (true);
 }
@@ -46,7 +45,7 @@ bool	searchAndReplace(const std::string &filename, const std::string &s1, const 
 int	main(int argc, char **argv)
 {
 	if (argc != 4)
-		std::cout << argv[0] << " <filename> <string to replace> <string that replace>" << std::endl;
+		return (std::cout << argv[0] << " <filename> <string to replace> <string that replace>" << std::endl, 1);
 	if (!searchAndReplace(argv[1], argv[2], argv[3]))
 		return (1);
 	return (0);
